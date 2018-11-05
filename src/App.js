a77de4af07c5af871da8e38926bcf6a0204e873b
Manuel Gegenhuber <Manuel.gegenhuber.mg@gmail.com>
2018-11-05 11:27:01 +0100

    change State of Component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,31 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    //state - only possible if class extends Component
+    state = {
+        persons: [
+            {name: 'dani', age: 99},
+            {name: 'viktor', age: 101},
+            {name: 'Mani', age: 66}
+        ]
+    }
+
+    switchNameHandler = () =>{
+        this.setState({
+            persons: [
+                {name: 'viktor', age: 12},
+                {name: 'Mani', age: 66},
+                {name: 'Dani', age: 111}
+            ]
+        });
+    }
+
     render() {
         return (
             <div className = "App" >
                 <h1 > Hi, I 'm a react app!</h1> 
-                <Person name="Dani" age="99"></Person>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} ></Person>
                 <Person name="Viktor" age="66">My Hobbies: Racing!</Person>
                 <Person name="Mani" age="111"></Person>
             </div>
