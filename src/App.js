b93133f7e605a48a12bfa5fd9f924ffdce0c194d
Manuel Gegenhuber <Manuel.gegenhuber.mg@gmail.com>
2018-11-27 22:01:22 +0100

    lists PART 2 - if and lists finished

--- src/App.js
+++ src/App.js
@@ -8,32 +8,44 @@ class App extends Component {
     //state - only possible if class extends Component
     state = {
         persons: [
-            {name: 'dani', age: 99},
-            {name: 'viktor', age: 101},
-            {name: 'Mani', age: 66}
+            {id: 'ergerg1',name: 'dani', age: 99},
+            {id: 'ergg5',name: 'viktor', age: 101},
+            {id: 'ergeg17b',name: 'Mani', age: 66}
         ],
         showPersons: false
     };
 
     deletePersonHandler = (personIndex) => {
-        const persons = this.state.persons;
+        //const persons = this.state.persons.slice(); //slice to copy the array -> otherwise bad practice because array = copy by reference
+        //alternative (new Approach)
+        const persons = [...this.state.persons];
         persons.splice(personIndex, 1);
         this.setState({persons: persons});
     };
 
-    nameChangedHandler = (event) =>{
-        this.setState({
-            persons: [
-                {name: 'Dani', age: 12},
-                {name: 'Viktor', age: 66},
-                {name: event.target.value, age: 111}
-            ]
+
+    nameChangedHandler = ( event, id ) => {
+        const personIndex = this.state.persons.findIndex(p => {
+          return p.id === id;
         });
-    };
+    
+        const person = {
+          ...this.state.persons[personIndex]
+        };
+    
+        // const person = Object.assign({}, this.state.persons[personIndex]);
+    
+        person.name = event.target.value;
+    
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+    
+        this.setState( {persons: persons} );
+      }
 
     togglePersonHandler = () => {
-        //const doesShow = this.state.showPersons; /*set doesShow to shoePerson (true or false)*/
-        this.setState({showPersons: !this.state.showPersons}); /*set shoePerson to opposite of doesShow (revert true and false)*/
+        const doesShow = this.state.showPersons; /*set doesShow to shoePerson (true or false)*/
+        this.setState({showPersons: !doesShow}); /*set shoePerson to opposite of doesShow (revert true and false)*/
     };
 
     //everytime react updates dom -> the whole render()- Method gets triggered
@@ -55,13 +67,17 @@ class App extends Component {
 
         //if state.showPersons = true -> render persons
         if(this.state.showPersons){
+
+            //map to convert normal array to jsx array
             persons = (
                 <div>
                     {this.state.persons.map((person, index) =>{
                         return <Person
                             click={() => {this.deletePersonHandler(index)}}
                             name={person.name}
-                            age={person.age}></Person>
+                            age={person.age}
+                            key={person.id}
+                            changed={(event) => {this.nameChangedHandler(event, person.id)}} />
                     })}
                 </div>
             );
