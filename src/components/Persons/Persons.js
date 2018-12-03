aecabaef978fca485ba1a4d0942a0c63cd0fdf17
Manuel Gegenhuber <Gegenhuber.Manuel@fronius.com>
2018-12-03 13:25:38 +0100

    after lifecycle hooks (in Persons.js, Person.js & App.js)

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,6 +3,13 @@ import Person from './Person/Person';
 
 
 class Persons extends Component{
+
+    /*
+        Call super(props)
+        DO:     Set up State
+        DON'T:  Cause Side-Effects (i.e. reach out for the web)
+    */
+
     constructor(props){
         //super(props) is necessary for the constructor to work
         super(props);
@@ -10,15 +17,62 @@ class Persons extends Component{
          Props: `, props);
     }
 
+
+    /*
+        DO:     Update State, last minute Optimization
+        DON'T:  Cause Side-Effects (i.e. reach out for the web)
+    */
+
     componentWillMount(){
         console.log(`[Persons.js] - Inside componentWillMount()
         `);
     }
 
-    componentDidMount(){
-        console.log(`[Persons.js] - Inside componentDidMount()`);
+    /**
+     * DO:      Sync State to props
+     * DON'T    Cause Side Effects 
+     */
+
+    componentWillReceiveProps(nextProps){
+        console.log('[Update Person.js] Inside componentWillReceiveProps');
     }
 
+    /**
+     * DO:      Decide whether to Continue or Not
+     * DON'T    Cause Side Effects 
+     * return false to not update and true to update
+     */
+
+    shouldComponentUpdate(nextProps, nextState){
+        console.log('[Update Person.js] Inside shouldComponentUpdate');
+        console.dir(nextProps);
+        console.dir(nextState);
+        return nextProps.persons !== this.props.persons;
+    }
+
+    /**
+     * DO:      Sync State to Props
+     * DON'T    Cause Side Effects 
+     */
+
+    componentWillUpdate(nextProps, nextState){
+        console.log('[Update Person.js] Inside componentWillUpdate');
+        console.dir(nextProps);
+        console.dir(nextState);
+    }
+
+    /**
+     * DO:      Cause Side Effects
+     * DON'T    Sync State to Props (triggers re-render) 
+     */
+
+    componentDidUpdate(){
+        console.log('[Update Person.js] Inside componentDidUpdate');
+    }
+
+    /*
+        Prepare & Structure your JSX Code
+    */
     render(){
 
         console.log(`[Persons.js] - Inside render()`);
@@ -35,6 +89,16 @@ class Persons extends Component{
                 })}
             </div>
         );
+    }
+
+
+    /*
+        DO:     Cause Side effects
+        DON'T:  Update State (or it triggers a re-render -> perfomance)
+    */
+
+    componentDidMount(){
+        console.log(`[Persons.js] - Inside componentDidMount()`);
     }
 } 
 
